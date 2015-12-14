bfc8a1bfb01bff109b7a60d8af3322b023dfd2be
DenDen047 <sh.mn.nat@gmail.com>
2015-12-15 00:04:55 +0900

    logistic_regression完成 costの変化をグラフで出力する

--- logistic_regression.py
+++ logistic_regression.py
@@ -25,29 +25,88 @@ def plot_data(X, y):
     )
 
 
-# データを読み込む
-# numpyの配列に格納
-data = np.genfromtxt("../logistic_regression/ex2data1.txt", delimiter=",")
-data_x = data[:, (0, 1)]
-data_y = data[:, 2]
+def main():
+    # データを読み込む
+    # numpyの配列に格納
+    data = np.genfromtxt("../logistic_regression/ex2data1.txt", delimiter=",")
+    data_x = data[:, (0, 1)]
+    data_y = data[:, 2]
 
-# 訓練データをプロット
-plt.figure(1)
-plot_data(data_x, data_y)
+    # 訓練データをプロット
+    # plt.figure(1)
+    # plot_data(data_x, data_y)
+
+    # データの個数を取得
+    m = len(data_x)
+
+    # 特徴量data_xに定数項を追加 .. x_0
+    data_x = np.hstack((np.ones((m, 1)), data_x))
+
+    # 訓練データを共有変数にする
+    X = theano.shared(
+        np.asarray(data_x, dtype=theano.config.floatX),
+        name='X',
+        borrow=True)
+    y = theano.shared(
+        np.asarray(data_y, dtype=theano.config.floatX),
+        name='y',
+        borrow=True)
+
+    # パラメータを共有変数とする
+    theta = theano.shared(
+        np.zeros(3, dtype=theano.config.floatX),
+        name='theta',
+        borrow=True)
+
+    # 仮説関数
+    h = T.nnet.sigmoid(T.dot(X, theta))
+
+    # コスト関数を定義する(sigmoid)
+    cost = (1.0 / m) * T.sum(
+        -y * T.log(h) - (1 - y) * T.log(1 - h)
+    )
+
+    # コスト関数の微分を定義
+    g_cost = theano.grad(cost=cost, wrt=theta)
+
+    # パラメータを更新
+    learning_rate = 0.001
+    updates = {theta: theta - learning_rate * g_cost}
+
+    # 訓練用の関数を定義
+    train_model = theano.function(
+        inputs=[],
+        outputs=cost,
+        updates=updates
+    )
+
+    # 繰り返す
+    cost_graph = []
+    interations = 300000
+    for iter in range(interations):
+        current_cost = train_model()
+        if iter % 100 == 0:
+            cost_graph.append(current_cost)
+            print iter, current_cost
+
+    # thetaの値を更新
+    t = theta.get_value()
+
+    # 決定境界を表示
+    plt.figure(1)
+    xmin, xmax = min(cost_graph), max(cost_graph)
+    xs = np.linspace(xmin, xmax, len(cost_graph))
+    ys = [y * 100 for y in cost_graph]
+    plt.plot(xs, ys, 'b-', label="cost graph")
+
+    # plotを表示
+    plt.xlabel("train times")
+    plt.ylabel("cost")
+    # plt.xlim((30, 100))
+    # plt.ylim((30, 100))
+    plt.legend()
+    plt.show()
 
 
-# コスト関数を定義する(sigmoid)
-# コスト関数の微分を定義
-# 勾配降下法
-# パラメータを更新
-# 訓練用の関数を定義
-# 繰り返す
-# 決定境界を表示
-
-# plotを表示
-plt.xlabel("x1")
-plt.ylabel("x2")
-plt.xlim((30, 100))
-plt.ylim((30, 100))
-plt.legend()
-plt.show()
+if __name__ == "__main__":
+    main()
